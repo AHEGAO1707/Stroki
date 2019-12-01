1614a232a944010c99bc72cc790b4d73fd20d4f5
AHEGAO1707 <egorka.alekseev.2015@mail.ru>
2019-12-02 02:07:44 +0300

    <= задача на строку

--- MaksDlina.java
+++ MaksDlina.java
@@ -6,7 +6,6 @@ public class MaksDlina {
         System.out.println("Введите строку для поиска последнего слова, имеющего максимальную длину => ");
         String k = in.nextLine();
         int n = 0;
-        String r = "";
         String[] b = k.split(" ");
         for (int i = 0; i < b.length; i++) {
             if (b[i].length() >= n) {
